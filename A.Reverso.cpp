e7e5194c0316950dcb91433d3a13b285576dd649
elramosq <elramosq@unjbg.edu.pe>
2026-06-19 02:48:12 -0500

    EjercicioA: Primer intento de Reverso

---
+++ A.Reverso.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+ int N, A[5], awx=0;
+	
+ cin>>N;
+	
+ for(int i=0;i<N;i++)
+ {
+  cin>>A[i];
+ }
+	
+ for (int i=0;i< N/2;i++)
+ {
+  awx = A[i];
+  A[i]=A[N-1-i];
+  A[N-1-i]=awx;
+ }
+	
+ for(int i=0;i<N;i++)
+ {
+  cout<<A[i]<<" ";
+ }
+	
+ return 0;
+}
