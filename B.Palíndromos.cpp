1eb4b6583d71bd24fa940ae8d4f2404ba1d5658e
elramosq <elramosq@unjbg.edu.pe>
2026-06-20 01:48:34 -0500

    EjercicioB: primer intento de Palíndromos

--- B.Palíndromos.cpp
+++ B.Palíndromos.cpp
@@ -1,12 +1,31 @@
-#include<iostream>
+#include <iostream>
+#include <cstring> 
 using namespace std;
-int main()
-{
- int N, A[5];
-	
- cin>>N;
-	
- for(int i=0;i<N;i++)
- {
-  cin>>A[i];
- }
+
+int main() {
+    int N;
+    cin >> N;
+
+    for (int i = 0; i < N; i++) {
+        char palabra[100]; 
+        cin >> palabra;
+
+        int longitud = strlen(palabra);
+        bool esPalindromo = true;
+
+      
+        for (int j = 0; j < longitud / 2; j++) {
+            if (palabra[j] != palabra[longitud - 1 - j]) {
+                esPalindromo = false;
+                break;
+            }
+        }
+
+        if (esPalindromo)
+            cout << "P" << endl;
+        else
+            cout << "NP" << endl;
+    }
+
+    return 0;
+}
